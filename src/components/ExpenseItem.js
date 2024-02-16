f816b2ccc3f4463729e9e1eedc37cf732bc91d1c
Anujeet Swain <anujeet98@gmail.com>
2024-02-17 04:33:16 +0530

    Initial commit. Created ExpenseItem components

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Feb 17 2024</div>
+      <div>
+        <h2>Hostel Fees</h2>
+        <div>Rs. 650</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
