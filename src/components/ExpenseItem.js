011bad776198f5bf30b010439d65fbfcb0bd7d3f
Anujeet Swain <anujeet98@gmail.com>
2024-02-17 05:02:45 +0530

    CSS added for expense component. Simulated Dymnaminc component values

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,22 +1,33 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
+    const expenseDate = new Date(2024, 2, 17);
+    const expenseTitle = 'Food';
+    const expenseAmount = 10; 
+    const expenseLocation = "Mumbai";
   return (
     <div>
-      <div>Feb 17 2024</div>
-      <div>
-        <h2>Food</h2>
-        <div>Rs. 10</div>
+      <div className="expense-item">
+        <div>{expenseDate.toISOString()}</div>
+        <h2>{expenseLocation}</h2>
+        <div className="expense-item__description">
+          <h2>{expenseTitle}</h2>
+          <div className="expense-item__price">Rs. {expenseAmount}</div>
+        </div>
       </div>
-
-      <div>Feb 17 2024</div>
-      <div>
-        <h2>Petrol</h2>
-        <div>Rs. 100</div>
+      <div className="expense-item">
+        <div>Feb 17 2024</div>
+        <div className="expense-item__description">
+          <h2>Petrol</h2>
+          <div className="expense-item__price">Rs. 100</div>
+        </div>
       </div>
-
-      <div>Feb 17 2024</div>
-      <div>
-        <h2>Movies</h2>
-        <div>Rs. 200</div>
+      <div className="expense-item">
+        <div>Feb 17 2024</div>
+        <div className="expense-item__description">
+          <h2>Movies</h2>
+          <div className="expense-item__price">Rs. 200</div>
+        </div>
       </div>
     </div>
   );
