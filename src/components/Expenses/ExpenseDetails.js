90192b7bf3efc3bf63db1ad46ad1ebd33c24700a
Anujeet Swain <anujeet98@gmail.com>
2024-02-18 04:16:03 +0530

    Added react useState hook to change state of components.

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,7 +1,18 @@
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import './ExpenseDetails.css';
 
 const ExpenseDetails = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const [price, setAmount] = useState(props.amount);
+  const updateTitleHandler = () => {
+      setTitle('updated!!!');
+      // console.log(title);
+  }
+  const updateAmountHandler = () => {
+      setAmount('100');
+      // console.log(amount);
+  }
   const deleteHandler = (event) => {
       const targetExpense = event.target.parentElement.parentElement;
       document.querySelector('.expenses').removeChild(targetExpense);
@@ -9,9 +20,11 @@ const ExpenseDetails = (props) => {
   return (
     <div className="expense-item__description">
       <h2>{props.location}</h2>
-      <h2>{props.title}</h2>
-      <Card className="expense-item__price">Rs. {props.amount}</Card>
+      <h2>{title}</h2>
+      <Card className="expense-item__price">Rs. {price}</Card>
       <button onClick={event => deleteHandler(event)}>Delete</button>
+      <button onClick={updateTitleHandler}>update title</button>
+      <button onClick={updateAmountHandler}>update price</button>
     </div>
   );
 }
