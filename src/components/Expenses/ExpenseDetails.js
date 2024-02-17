fc3f6af19ebddb5af1ed641e675830880ed183f2
Anujeet Swain <anujeet98@gmail.com>
2024-02-18 01:03:54 +0530

    Added Delete buttons. Attached Delete EventHandling with DOM mainpulation

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -2,11 +2,16 @@ import Card from '../UI/Card';
 import './ExpenseDetails.css';
 
 const ExpenseDetails = (props) => {
+  const deleteHandler = (event) => {
+      const targetExpense = event.target.parentElement.parentElement;
+      document.querySelector('.expenses').removeChild(targetExpense);
+  }
   return (
     <div className="expense-item__description">
       <h2>{props.location}</h2>
       <h2>{props.title}</h2>
       <Card className="expense-item__price">Rs. {props.amount}</Card>
+      <button onClick={event => deleteHandler(event)}>Delete</button>
     </div>
   );
 }
