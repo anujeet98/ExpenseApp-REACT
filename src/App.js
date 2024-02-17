b907d492ac61ea4f8fa3f24b92dd989c846dbec6
Anujeet Swain <anujeet98@gmail.com>
2024-02-17 05:45:07 +0530

    Using props; Dynamic content rendering using looping

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import ExpenseItem from "./components/ExpenseItem";
 
-
 function App() {
+  const expenses = [
+    {
+      id: "1",
+      date: new Date(2024, 2, 17),
+      location: "Mumbai",
+      title: "Food",
+      amount: 10,
+    },
+    {
+      id: "2",
+      date: new Date(2024, 2, 3),
+      location: "Pune",
+      title: "Petrol",
+      amount: 100,
+    },
+    {
+      id: "3",
+      date: new Date(2023, 12, 23),
+      location: "Delhi",
+      title: "Movie",
+      amount: 200,
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      {
+        expenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            date={expense.date}
+            location={expense.location}
+            title={expense.title}
+            amount={expense.amount}
+          ></ExpenseItem>
+        ))
+      }
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,33 +1,13 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    const expenseDate = new Date(2024, 2, 17);
-    const expenseTitle = 'Food';
-    const expenseAmount = 10; 
-    const expenseLocation = "Mumbai";
+function ExpenseItem(props) {
   return (
-    <div>
-      <div className="expense-item">
-        <div>{expenseDate.toISOString()}</div>
-        <h2>{expenseLocation}</h2>
-        <div className="expense-item__description">
-          <h2>{expenseTitle}</h2>
-          <div className="expense-item__price">Rs. {expenseAmount}</div>
-        </div>
-      </div>
-      <div className="expense-item">
-        <div>Feb 17 2024</div>
-        <div className="expense-item__description">
-          <h2>Petrol</h2>
-          <div className="expense-item__price">Rs. 100</div>
-        </div>
-      </div>
-      <div className="expense-item">
-        <div>Feb 17 2024</div>
-        <div className="expense-item__description">
-          <h2>Movies</h2>
-          <div className="expense-item__price">Rs. 200</div>
-        </div>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <h2>{props.location}</h2>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">Rs. {props.amount}</div>
       </div>
     </div>
   );
