43452c232aa20ce7fb27f084d7e751a249e49ebc
Anujeet Swain <anujeet98@gmail.com>
2024-02-18 06:08:03 +0530

    Removed components FormInput, FormButton; Added multiple state change events

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,18 +1,29 @@
 // import { useState } from "react";
+import { useState } from "react";
 import Card from "../UI/Card"
-import FormButton from "./FormButton";
-import FormInput from "./FormInput";
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+    const [title, enteredTitle] = useState('');
+    const [amount, enteredAmount] = useState('');
+    const [date, enteredDate] = useState();
+
+    const inputChange = (event) => {
+        if(event.target.name === "title")
+            enteredTitle(event.target.value);
+        else if(event.target.name === "amount")
+            enteredAmount(event.target.value);
+        else if(event.target.name === "date")
+            enteredDate(event.target.value);
+    }
     return (
         <Card className="formContainer">
             <div className="expenseForm">
-                <FormInput type="text" placeholder="Add Expense Title Here.."/>
-                <FormInput type="number" placeholder="Add Expense Amount Here.."/>
-                <FormInput type="date"/>
+                <input onChange={inputChange} type="text" name="title" placeholder="Add Expense Title Here.." value={title}/>
+                <input onChange={inputChange} type="number" name="amount" placeholder="Add Expense Amount Here.." value={amount}/>
+                <input onChange={inputChange} type="date" name="date" value={date}/>
             </div>
-            <FormButton value="Add Expense"/>
+            <button>Add Expense</button>
         </Card>
     )
 };
--- src/components/ExpenseForm/FormButton.js
+++
@@ -1,9 +0,0 @@
-import './FormButton.css'
-
-const FormButton = (props) => {
-    return (
-        <button className="formButton">{props.value}</button>
-    )
-};
-
-export default FormButton;
